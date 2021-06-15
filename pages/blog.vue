cc1615a6ebdffca82ed7b1d82f60bf37b42ff6d8
nullworkbench <nullworkbench@gmail.com>
2021-06-15 19:18:29 +0900

    blogのqiita一覧のCSSを調整

--- pages/blog.vue
+++ pages/blog.vue
@@ -3,17 +3,25 @@
     <h2>Blog</h2>
     <div class="mainWrapper">
       <main>
-        <div v-if="$fetchState.pending">Fetching Qiita Posts...</div>
-        <div v-else-if="$fetchState.error">
-          An Error Occured When Fetching Qiita Posts.
-        </div>
-        <div v-else>
-          <h3>{{ qiitaPosts[0]['title'] }}</h3>
-          <p>{{ qiitaPosts[0]['url'] }}</p>
-          <p>{{ qiitaPosts[0]['likes_count'] }}</p>
-          <p>{{ qiitaPosts[0]['tags'] }}</p>
-          <p>{{ qiitaPosts[0]['created_at'] }}</p>
-        </div>
+        <section id="sectionQiita">
+          <div v-if="$fetchState.pending">Fetching Qiita Posts...</div>
+          <div v-else-if="$fetchState.error">
+            An Error Occured When Fetching Qiita Posts.
+          </div>
+          <div v-else>
+            <div v-for="(post, idx) in qiitaPosts" :key="idx" class="qiitaPost">
+              <h3>{{ post['title'] }}</h3>
+              <p>{{ formatDate(post['created_at']) }}</p>
+              <div class="lgtm">LGTM {{ post['likes_count'] }}</div>
+              <div class="tags">
+                <div v-for="(tag, tagIdx) in post['tags']" :key="tagIdx">
+                  {{ tag.name }}
+                </div>
+              </div>
+              <a :href="post['url']" target="_blank"></a>
+            </div>
+          </div>
+        </section>
       </main>
     </div>
   </div>
@@ -35,8 +43,19 @@ export default {
       },
     })
   },
-  mounted() {},
-  methods: {},
+  methods: {
+    formatDate(str) {
+      const date = new Date(str)
+      return (
+        date.getFullYear() +
+        '年' +
+        date.getMonth() +
+        '月' +
+        date.getDay() +
+        '日'
+      )
+    },
+  },
 }
 </script>
 
@@ -46,5 +65,66 @@ h2 {
   top: 5rem;
   left: 50%;
   transform: translateX(-50%);
+}
+
+.mainWrapper {
+  padding-top: 13rem;
+}
+main {
+  max-width: 1000px;
+  margin: 0 auto;
+}
+
+// Qiita
+#sectionQiita {
+  .qiitaPost {
+    position: relative;
+    background: #f9f9f9;
+    width: 80%;
+    max-width: 700px;
+    margin: 0 auto;
+    margin-bottom: 2rem;
+    padding: 2rem;
+    border-radius: 1rem;
+    box-shadow: 0 0 10px #eee;
+
+    > p {
+      margin-bottom: 1rem;
+    }
+
+    a {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+    }
+  }
+  h3 {
+    margin-bottom: 0.2rem;
+  }
+
+  .lgtm {
+    display: inline-block;
+    background: #55c500;
+    color: #fff;
+    font-size: 0.8rem;
+    font-weight: 800;
+    border-radius: 0.5rem;
+    padding: 0.5rem;
+    padding-right: 0.6rem;
+    margin-right: 0.5rem;
+  }
+  .tags {
+    display: inline-block;
+    font-size: 0.8rem;
+    > div {
+      display: inline-block;
+      background: #bde7ef;
+      padding: 0.5rem 1rem;
+      border-radius: 0.5rem;
+      margin-right: 0.5rem;
+    }
+  }
 }
 </style>
