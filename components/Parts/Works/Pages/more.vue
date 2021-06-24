487dabb0f7a415fd782ee2d549ae78a1a5dc361b
nullworkbench <nullworkbench@gmail.com>
2021-06-24 17:55:38 +0900

    worksにMore の詳細を追加

--- components/Parts/Works/Pages/more.vue
+++ components/Parts/Works/Pages/more.vue
@@ -1,6 +1,20 @@
 <template>
   <div>
     <h3>More</h3>
+
+    <div class="basicInfo">
+      <ul>
+        <li>クライアント：神奈川県立元石川高等学校 More 様</li>
+        <li>開発期間：2020/04/04 ~ 2021/05/15</li>
+        <li>担当：企画、デザイン、開発、保守</li>
+        <li>開発技術：Nuxt.js</li>
+        <li>
+          参考リンク：<a href="https://more-motoishikawa.cf" target="_blank"
+            >More 公式サイト</a
+          >
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -8,6 +22,6 @@
 export default {}
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 @import '@/assets/scss/WorksPagesCommon.scss';
 </style>
