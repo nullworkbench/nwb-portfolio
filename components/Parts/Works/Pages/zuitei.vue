dc8f461096c875ab9f63c631a8dedde0cb623190
nullworkbench <nullworkbench@gmail.com>
2021-06-24 17:08:05 +0900

    worksに瑞亭を追加

--- components/Parts/Works/Pages/zuitei.vue
+++ components/Parts/Works/Pages/zuitei.vue
@@ -1,6 +1,50 @@
 <template>
-  <div>
-    <h3>懐石料亭 瑞亭</h3>
+  <div class="container">
+    <span>懐石料亭 瑞亭 公式サイト</span>
+    <h3>瑞亭</h3>
+
+    <div class="basicInfo">
+      <ul>
+        <li>クライアント：懐石料亭 瑞亭 様</li>
+        <li>開発期間：2021/02/22 ~ 2021/04/25</li>
+        <li>担当：営業、デザイン、開発、保守</li>
+        <li>開発技術：HTML, CSS, Nuxt.js, WordPress</li>
+        <li>
+          参考リンク：<a
+            href="https://tamaplaza-washoku-zuitei.com"
+            target="_blank"
+            >瑞亭 公式サイト</a
+          >
+        </li>
+      </ul>
+    </div>
+
+    <section>
+      <h4>開発のきっかけ</h4>
+      <p>
+        私の友人がこちらの料亭でアルバイトをしておりその友人からの紹介でご依頼いただいたことがきっかけ。
+      </p>
+      <p>
+        現状のホームページをリニューアルしたいとのことで、現状把握のヒアリングから始め現状の不満点、新ホームページへの期待などを伺い、正式にお引き受けした。
+      </p>
+    </section>
+
+    <section>
+      <h4>アピールポイント</h4>
+      <p>
+        ユーザーが最初に料亭と接触する際の印象を高めるため、既存の情報を移行するだけでなく、写真や動画撮影をしてトップページのカバー動画などを作成し、ページを開いた時にインパクトを与えられるよう設計した。
+      </p>
+      <p>
+        また、ランニングコストを抑えるためにクラウドではなく静的なレンタルサーバーを使用した。フロントエンドはNuxt.jsによるSSG、バックエンドにPHP及びWordPressを使用することで、価格面でも保守面でも低ランニングコストを実現しクライアントに大変ご好評いただいた。
+      </p>
+    </section>
+
+    <section>
+      <h4>懐石料亭 瑞亭 公式サイト</h4>
+      <a href="https://tamaplaza-washoku-zuitei.com" target="_blank"
+        >瑞亭 公式サイト</a
+      >
+    </section>
   </div>
 </template>
 
@@ -8,6 +52,6 @@
 export default {}
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 @import '@/assets/scss/WorksPagesCommon.scss';
 </style>
