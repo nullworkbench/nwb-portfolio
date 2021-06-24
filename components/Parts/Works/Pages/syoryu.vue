4cc370b819f6167eed806c120cefd7404cf27ac7
nullworkbench <nullworkbench@gmail.com>
2021-06-24 17:47:25 +0900

    worksに松隆を追加

--- components/Parts/Works/Pages/syoryu.vue
+++ components/Parts/Works/Pages/syoryu.vue
@@ -1,6 +1,42 @@
 <template>
-  <div>
+  <div class="container">
     <h3>松隆</h3>
+
+    <div class="basicInfo">
+      <ul>
+        <li>クライアント：有限会社 松隆 様</li>
+        <li>開発期間：2021/12/09 ~ 2020/04/17</li>
+        <li>担当：営業、デザイン、開発、保守</li>
+        <li>開発技術：HTML, CSS, WordPress, BASE(ネットショップ), Nuxt.js</li>
+        <li>
+          参考リンク：<a href="https://syoryu-kunsei.com" target="_blank"
+            >松隆 公式サイト</a
+          >
+        </li>
+      </ul>
+    </div>
+
+    <section>
+      <h4>開発のきっかけ</h4>
+      <p>
+        友人の親戚で、電話のみで注文を受注しているお店があると聞き、ネットショップの設立を提案。「ネットショップと合わせて公式サイト、どちらもぜひお願いしたい」とのことで正式にご依頼いただいた。
+      </p>
+    </section>
+
+    <section>
+      <h4>アピールポイント</h4>
+      <p>
+        WordPressを主に使用して開発し、クライアントが自由に情報を更新できる仕組みを取ることでランニングコストを削減した。
+      </p>
+      <p>
+        その他にもネットショップ作成サービスであるBASE、ブログ、Instagramなどの動的コンテンツを公式サイトに繋げることでクライアントご自身でホームページの情報の新鮮度を保てる仕組みとした。
+      </p>
+    </section>
+
+    <section>
+      <h4>松隆 公式サイト</h4>
+      <a href="https://syoryu-kunsei.com" target="_blank">松隆 公式サイト</a>
+    </section>
   </div>
 </template>
 
@@ -8,6 +44,6 @@
 export default {}
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 @import '@/assets/scss/WorksPagesCommon.scss';
 </style>
