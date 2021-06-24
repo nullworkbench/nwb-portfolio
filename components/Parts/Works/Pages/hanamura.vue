5937ba03eb5d70eede0dd1ec4e80af0077ad7932
nullworkbench <nullworkbench@gmail.com>
2021-06-24 17:59:46 +0900

    worksに花むらの詳細を追加

--- components/Parts/Works/Pages/hanamura.vue
+++ components/Parts/Works/Pages/hanamura.vue
@@ -1,6 +1,20 @@
 <template>
   <div>
     <h3>花むら</h3>
+
+    <div class="basicInfo">
+      <ul>
+        <li>クライアント：蕎麦処 花むら 様</li>
+        <li>開発期間：2020/04/30 ~ 2020/05/11</li>
+        <li>担当：営業、デザイン、開発、保守</li>
+        <li>開発技術：Nuxt.js</li>
+        <li>
+          参考リンク：<a href="https://hanamura.cf" target="_blank"
+            >花むら 公式サイト</a
+          >
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -8,6 +22,6 @@
 export default {}
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 @import '@/assets/scss/WorksPagesCommon.scss';
 </style>
