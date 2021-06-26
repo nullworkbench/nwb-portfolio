f9e83d5373691b53e0f94e40dc4966039a25a754
nullworkbench <nullworkbench@gmail.com>
2021-06-26 20:26:31 +0900

    libraryの準備ページを追加

---
+++ pages/library.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container">
+    <h2>Library</h2>
+
+    <main>
+      <p>Coming soon...</p>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+.container {
+  font-family: 'Comfortaa', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-weight: 200;
+  font-size: 1rem;
+  background: rgba(255, 255, 255, 0.5);
+}
+h2 {
+  padding-top: 7rem;
+  text-align: center;
+}
+
+main {
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  width: 100%;
+  margin: 0 auto;
+  text-align: center;
+}
+</style>
