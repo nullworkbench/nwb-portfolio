eab7e614d1725661c904991b2e7a85105724c063
nullworkbench <nullworkbench@gmail.com>
2021-06-26 23:24:09 +0900

    7122477

---
+++ _nuxt/static/1624717391/about/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnullworkbench.github.io\u002F_nuxt\u002Fstatic\u002F1624717391",layout:"default",error:a,serverRendered:true,routePath:"\u002Fabout",config:{_app:{basePath:"\u002Fnullworkbench.github.io\u002F",assetsPath:"\u002Fnullworkbench.github.io\u002F_nuxt\u002F",cdnURL:a}}}}(null));
